c218203d9f05708ed393d8fd4e3bf58e2a414b3d
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-03-06 19:57:31 -0600

    added listeners & functions

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("hello");
+
+function executeThisCodeAfterFileLoaded(){
+  console.log("It Worked!");
+}
+
+function executeThisCodeAfterFileFails(){
+  console.log("It failed...");
+}
+
+var myRequest = new XMLHttpRequest();
+myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+myRequest.addEventListener("error", executeThisCodeAfterFileFails);
+myRequest.open("GET", "dinosaurs.json");
+myRequest.send();
+console.log("myRequest", myRequest);
+
+// HTTP VERBS
+// GET - get information
+// POST - post exisiting information
+// PUT
+// DELETE
