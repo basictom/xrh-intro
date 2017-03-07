d5fdede8fe6bc06566d325b834892cd357b86291
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-03-06 21:18:01 -0600

    class ovah

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-console.log("hello");
+console.log("First line in JS file:", Date.now());
+
+var output = document.getElementById("dino-container");
+
+function makeDom(xhrData){
+  // output.innerHTML = xhrData;
+  var dinoString = "";
+  var currentDino = "";
+  for(var i=0; i<xhrData.dinosaurs.length; i++){
+    currentDino = xhrData.dinosaurs[i];
+    dinoString += `<div class="col-sm-6 col-md-4">`;
+    dinoString += `<div class="thumbnail">`;
+    dinoString += `<img src="${currentDino.url}" alt="${currentDino.name}">`
+    dinoString += `<div class="caption">`;
+    dinoString += `<h3>${currentDino.name}</h3>`
+    dinoString += `<p>${currentDino.type}</p>`
+    dinoString += `<p>${currentDino.food}</p></div></div></div>`;
+  }
+  output.innerHTML = dinoString;
+};
 
 function executeThisCodeAfterFileLoaded(){
-  console.log("It Worked!");
+  console.log("Data returned:", Date.now());
+  var data = JSON.parse(this.responseText)
+  // console.log("It Worked!", data);
+  makeDom(data);
 }
 
 function executeThisCodeAfterFileFails(){
@@ -13,10 +35,11 @@ myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
 myRequest.addEventListener("error", executeThisCodeAfterFileFails);
 myRequest.open("GET", "dinosaurs.json");
 myRequest.send();
-console.log("myRequest", myRequest);
+// console.log("myRequest", myRequest);
 
 // HTTP VERBS
 // GET - get information
 // POST - post exisiting information
 // PUT
 // DELETE
+console.log("Last line in JS file:", Date.now());
